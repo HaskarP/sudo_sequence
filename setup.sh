d4efc03c2223ff3edafff773bd1a20eb41427780
Nathan Corbyn <me@nathancorbyn.com>
2018-10-20 11:45:50 +0100

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 pip3 install virtualenv
-virtualenv venv
+virtualenv --system-site-packages -p python3 venv
 source ./venv/bin/activate
+pip3 install --upgrade pip
 pip3 install -U Flask
 pip3 install -U Dash
 pip3 install -U tensorflow
