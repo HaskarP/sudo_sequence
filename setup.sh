73bedcc24bba90a47f036d7ccf30c076a9074ad6
Nathan Corbyn <me@nathancorbyn.com>
2018-10-20 11:32:23 +0100

    Add dependencies

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-pip install virtualenv
+pip3 install virtualenv
 virtualenv venv
 source ./venv/bin/activate
-pip install -U Flask
-pip install -U Dash
+pip3 install -U Flask
+pip3 install -U Dash
+pip3 install -U tensorflow
+pip3 install -U tensorflow-gpu
+pip3 install -U numpy
 deactivate
