94f55bea47fe76ca269c7f97834a260123d0858a
Thomas Crawley <tmdc257@gmail.com>
2018-10-21 13:01:46 +0100

    Add patch to json conversion

---
+++ src/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from dash import Dash
+import daru
+import json
+import operator
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+
+server = Flask(__name__)
+app = Dash(__name__, server=server, url_base_pathname='/')
+
+app.layout = html.Div(
+    children=[html.Button('Submit', id='button')]
+)
+
+def json_response(json_string):
+    response = server.response_class(
+        response=json_string,
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+@server.route("/api/patches/")
+def patches():
+
+    num_patches = 200
+
+    indices = daru.read_index("daru.idaru")
+    metric_sorted = sorted(indices.items(), key=operator.itemgetter(1), reverse=True)
+    top_indices = metric_sorted[:num_patches:]
+
+    to_json = dict(top_indices)
+
+    json_string = json.dumps(to_json)
+
+    return json_response(json_string)
+
+
+if __name__ == "__main__":
+    server.run()
